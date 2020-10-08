183024cbfd4582f85706a6e3989ea86164d2a2dc
wikugw <wikugw@gmail.com>
2020-10-08 14:16:47 +0700

    scrollview

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { setStatusBarNetworkActivityIndicatorVisible, StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
+  const [pokemons, setPokemon] = useState([
+    { name: 'charizard', key: 1 },
+    { name: 'turtwig', key: 2 },
+    { name: 'blaziken', key: 3 },
+    { name: 'rayquaza', key: 4 },
+    { name: 'infernape', key: 5 },
+    { name: 'empoleon', key: 6 },
+    { name: 'bulbasaur', key: 7 },
+    { name: 'groudon', key: 8 },
+    { name: 'ambipom', key: 9 }
+  ]);
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello world</Text>
-      </View>
-      <View style={styles.body}>
-        <Text>Lorem ipsum haha haha meja</Text>
-        <Text>Lorem ipsum haha haha meja</Text>
-        <Text>Lorem ipsum haha haha meja</Text>
-        <Text>Lorem ipsum haha haha meja</Text>
-      </View>
+      <ScrollView>
+        {pokemons.map((pokemon) => {
+          return (
+            <View key={pokemon.key}>
+              <Text style={styles.text}>{pokemon.name}</Text>
+            </View>
+          )
+        })}
+      </ScrollView>
     </View>
   );
 }
@@ -22,18 +34,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20
   },
-  header: {
-    backgroundColor: '#66b933',
-    padding: 20
-  },
-  boldText: {
-    fontWeight: 'bold'
-  },
-  body: {
-    backgroundColor: '#696969',
-    padding: 20
+  text: {
+    color: 'green',
+    padding: 40,
+    borderWidth: 1,
+    borderColor: 'green',
+    margin: 5
   }
 });
